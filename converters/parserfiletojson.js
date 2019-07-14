c5edde8b6d5c45a6da37193c1bddb48d733b6231
kandy16 <kandy.from1986@gmail.com>
2019-07-14 23:07:41 +0200

    Functionable GUI
    convertors to create json file from parser tree file with the help of translator document
    Main comment and meaning, tree comment update
    Ability of add or delete main and translate trees
    All tree operations update the respective data
    save the json
    German and Tamil translations for meaning is put together

---
+++ converters/parserfiletojson.js
@@ -0,0 +1,119 @@
+class parserfiletojson{
+
+    constructor(input){
+        this.input = input;
+        /* Input format
+        {
+            'parser':{
+                'content':'',
+                'language':''
+            },
+            'translation':{
+                'content':'',
+                'languages':[]
+            }
+        };*/
+    }
+    
+    convert(){
+        /* Output format
+        var output = [{
+            "meaning":"",
+            "comment":"",
+            "ta": [
+            {"gloss":[], "comment":"", "tree":parseContent}
+            ]            
+        }];
+        */
+        
+        //Update the translation content
+        var translationContent = undefined;
+        var languageFound = false;
+        if(this.input.translation.content){
+            translationContent = {};
+            var temp = this.input.translation.content.split('\n');
+            var lines = [];
+            for(let i in temp){
+                lines.push(temp[i].trim());
+            }
+            var header = lines[0].split('\t');
+            for(let i in header){
+                translationContent[header[i]] = [];
+            }
+            var content = lines
+            for(let i=1;i<lines.length;i++){
+                var line = lines[i];
+                var contents = line.split('\t');
+                
+                for(let j in contents){
+                    try{
+                        translationContent[header[j]].push(contents[j].trim());    
+                    }catch(err){
+                        console.log(contents);
+                        console.log(j);
+                    }
+                }
+            }
+            languageFound = Object.keys(translationContent).includes(this.input.parser.language);
+        }
+        
+        languageData = parseWholeBracketsFile(this.input.parser.content,
+                                              this.input.parser.language);
+
+        for(let i in languageData){
+            
+            //Go through the parser content
+            // Duplicate and update for all languages
+            var item = languageData[i];
+            var inputLangData = item[this.input.parser.language];
+            var textData = JSON.stringify(inputLangData,
+                                          function(key, value){if(key == 'id'){return undefined} return value;});
+            for (let j in this.input.translation.languages){
+                item[this.input.translation.languages[j]] = JSON.parse(textData);
+            }
+            
+            // if the language is found in translated document
+            // compare the sentence and if found update their comments
+            
+            if(languageFound){
+                var sentenceList = [];
+                try{
+                    if(i == 98){
+                        console.log('may be error');
+                    }
+                    getSentence(inputLangData[0].tree, sentenceList);        
+                }
+                catch(err){
+                    console.log(inputLangData);
+                    console.log(i);
+                }
+                
+                var sentence = sentenceList.join(' ').trim();
+                
+                var matchIndex = translationContent[this.input.parser.language].indexOf(sentence);
+                if(matchIndex != -1){
+                    item.meaning = translationContent['en'][matchIndex];
+                    item.comment = translationContent['en'][matchIndex];
+                    
+                    for (let j in this.input.translation.languages){
+                        if(Object.keys(translationContent).includes(this.input.translation.languages[j])){
+                            item[this.input.translation.languages[j]][0].comment = translationContent[this.input.translation.languages[j]][matchIndex]; 
+                        }
+                    }
+                }
+            }
+        }
+        
+        return languageData;
+    }
+}
+
+function getSentence(node, sentenceList){
+    if(node.children){
+        for(let i in node.children){
+            getSentence(node.children[i], sentenceList);
+        }
+    } else{
+        sentenceList.push(node.label);
+    }
+}
--- index.js
+++ index.js
@@ -1,66 +1,63 @@
-console.log('my simple file is working !!');
-
-var languageData =  {};
-var languages = ['de','ta','en'];
-var chosenLanguage = 'ta';
-var treegui = undefined;
-
-function changeLanguage(input){
-    chosenLanguage = input;
-    convertText(languageData);
-}
-
-function convertText(languageData){
-    
-    var sentenceText = '<ul>';
-    
-    for (var i in languageData){
-        var sentenceObj = languageData[i];
-        var sentence =  (sentenceObj.meaning)? sentence.meaning:'There is a meaning!!!';
-        sentenceText += '<button class="list-group-item" onclick=showGraph('+i+')>'+sentence+'</button>';
-    }
-    
-    sentenceText += '</ul>';
-    displayElement = document.querySelector('#items');
-    displayElement.innerHTML = sentenceText;
-}
-
-function showGraph(index){
-    var sentenceObj = languageData[index];
-    var sentenceLangObj = sentenceObj[chosenLanguage];
-    
-    treegui = new TreeGUI('graph');
-    treegui.loadTreeFunc(sentenceLangObj[0].tree);
-}
-
-
-//displayElement = document.querySelector('#display');
-//displayElement.value = JSON.stringify(treeData);
-
- 
 // Check for the various File API support.
 if (window.File && window.FileReader && window.FileList && window.Blob) {
+    console.log('my browser has support for HTML5 APIs :-) !!');
     
-    var parserFileText = '';
+    var languages = ['de','ta','en'];
+    var jsonConverterSection = document.querySelector('#tojson');
+
+    var input = {};
     function handleParseFileSelect(evt){
         var files = evt.target.files; // FileList object
         console.log(files);
         var reader = new FileReader();
         reader.onload = function(event){
-            parserFileText = event.target.result;
-            document.querySelector('#displaytojson').innerHTML = 'File read succcessfully';
+            input.parser = {};
+            input.parser.content = event.target.result;
+            input.parser.language = languages[0];
+            jsonConverterSection.querySelector('.display').innerHTML = 'Parser File read succcessfully';
         }
-        reader.readAsText(files[0]);
+        reader.readAsText(files[0],'utf-8');
+    }   
+    jsonConverterSection.querySelector('.loadfile').addEventListener('change', handleParseFileSelect, false);
+    
+    var languagesSelectionElement = '';
+    for(let i in languages){
+        languagesSelectionElement += '<option selected>'+languages[i]+'</option>';
     }
+    jsonConverterSection.querySelector('.loadlang').innerHTML = languagesSelectionElement;
+    
+    function handleTranslateFileSelect(evt){
+        var files = evt.target.files; // FileList object
+        console.log(files);
+        var reader = new FileReader();
+        reader.onload = function(event){
+            input.translation = {};
+            input.translation.content = event.target.result;
+            input.translation.languages = languages;
+            jsonConverterSection.querySelector('.display').innerHTML = 'Translation File read succcessfully';
+        }
+        reader.readAsText(files[0],'utf-8');
+    }   
+    jsonConverterSection.querySelector('.translatefile').addEventListener('change', handleTranslateFileSelect, false); 
+    jsonConverterSection.querySelector('.translatelang').innerHTML = languagesSelectionElement;
     
     function saveToJSON(e){
         e.preventDefault();
         
-        languageData = parseWholeBracketsFile(parserFileText, document.querySelector('#langlist').value);
-
-        var text = document.querySelector("textarea");
+        input.parser.language = jsonConverterSection.querySelector('.loadlang').value;
+        input.translation.languages = [];
+        let selectedOptions = jsonConverterSection.querySelector('.translatelang').selectedOptions;
+        for(let i in selectedOptions){
+            if(!isNaN(i)){ //some properties are not numeric
+                input.translation.languages.push(selectedOptions[i].value);
+            }
+        }
+            
+        let tojsonObj = new parserfiletojson(input);
+        let saveData = tojsonObj.convert();
         
-        var blob = new Blob([JSON.stringify(languageData)], {
+        var blob = new Blob([JSON.stringify(languageData,
+                                          function(key, value){if(key == 'id'){return undefined} return value;})], {
         "type": "application/json"
         });
 
@@ -70,29 +67,136 @@ if (window.File && window.FileReader && window.FileList && window.Blob) {
         document.body.appendChild(a);
         a.click();
         document.body.removeChild(a);
+        jsonConverterSection.querySelector('.display').innerHTML = 'Save to JSON read succcessfully';
+        
     }
+    jsonConverterSection.querySelector('.save').onclick = saveToJSON;  
     
-    document.querySelector('#loadparsefile').addEventListener('change', handleParseFileSelect, false);
-    document.querySelector('#savetojson').onclick = saveToJSON;    
+    var languageData =  {};
+    var chosenLanguage = 'ta';
+    var treegui = undefined;
+    var currentSelected = undefined;
+    var modifiedHistory = [];
+    
+    var jsonViewSelectSection = document.querySelector('#view-selectjson');
+    var jsonListSelectSection = document.querySelector('#list-selectjson');
+    var jsonDrawSelectSection = document.querySelector('#draw-selectjson');
 
-
-    function handleFileSelect(evt) {
+    function handleParserFileToViewSelect(evt) {
         var files = evt.target.files; // FileList object
         console.log(files);
         var reader = new FileReader();
         reader.onload = function(event){
-            languageData = JSON.parse(event.target.result);
-            convertText(languageData);
-            console.log('File read succcessfully');
+            languageData = JSON.parse(event.target.result, function(key, value){
+                if(key == 'id')
+                {
+                    return undefined;
+                }
+                return value;
+            });
+            buildList(languageData, chosenLanguage);
+            showGraph([0,0]);
+            console.log('Parser File to view is read succcessfully !!!');
         }
-        reader.readAsText(files[0]);
+        reader.readAsText(files[0],'utf-8');
     }
+    function buildList(languageData, chosenLanguage){
+        var sentenceText = '<ul>';
+
+        for (var i in languageData){
+            var sentenceObj = languageData[i];
+            var sentence = (Number(i)+1)+') ' + ((sentenceObj.meaning)? sentenceObj.meaning:'Meaning missing. Enter!');
+            sentenceText += '<div class="list-group-item">';
+            sentenceText += '<label>'+sentence+'</label>';
+            var sentenceLangObj = sentenceObj[chosenLanguage];
+            
+            for(var j in sentenceLangObj){
+                sentenceText += '<button onclick=showGraph('+'['+i+','+j+']'+')>'+sentenceLangObj[j].comment+'</button>';
+            }
+            sentenceText += '</div>';
+        }
+
+        sentenceText += '</ul>';
+        displayElement = jsonListSelectSection.querySelector('.list-group');
+        displayElement.innerHTML = sentenceText;
+        
+
+    }
+    function showGraph(indexes){
+        currentSelected = indexes;
+        
+        var sentenceObj = languageData[indexes[0]];
+        var sentenceLangObj = sentenceObj[chosenLanguage];
+
+        treegui = new TreeGUI('graph');
+        treegui.loadTreeFunc(sentenceLangObj[indexes[1]].tree);
+        
+        var textAreaList =  jsonViewSelectSection.querySelectorAll('textarea');
+        textAreaList[0].value = ((sentenceObj.meaning)? sentenceObj.meaning:'Meaning missing. Enter!');
+        textAreaList[1].value = ((sentenceObj.comment)? sentenceObj.comment:'Comment missing. Enter!');
+        
+        jsonDrawSelectSection.querySelector('textarea').value = sentenceLangObj[indexes[1]].comment;
+    }
+
+    jsonViewSelectSection.querySelector('.loadjson').addEventListener('change', handleParserFileToViewSelect, false);
     
+    function mainMeaningUpdate(){
+        if(languageData && currentSelected){
+            var sentenceObj = languageData[currentSelected[0]];
+            sentenceObj.meaning = jsonViewSelectSection.querySelectorAll('textarea')[0].value;
+        }
+    }
+    jsonViewSelectSection.querySelector('.main-meaning').onclick = mainMeaningUpdate;
+    
+    function mainCommentUpdate(){
+        if(languageData && currentSelected ){
+            var sentenceObj = languageData[currentSelected[0]];
+            sentenceObj.comment = jsonViewSelectSection.querySelectorAll('textarea')[1].value;
+        }
+    }
+    jsonViewSelectSection.querySelector('.main-comment').onclick = mainCommentUpdate;
+    
+    function addMainTree(){
+        if(currentSelected){
+            var tempData = JSON.stringify(languageData[currentSelected[0]]);
+            languageData.splice(currentSelected[0], 0, JSON.parse(tempData));
+            
+            buildList(languageData, chosenLanguage);
+            showGraph(currentSelected);
+        } else{
+            console.log('Choose a node first!');
+        }
+    }
+    jsonViewSelectSection.querySelector('.add').onclick = addMainTree;
+    
+    function deleteMainTree(){
+        if(currentSelected){
+            languageData.splice(currentSelected[0], 1);
+            
+            buildList(languageData, chosenLanguage);
+            showGraph(currentSelected);
+        } else{
+            console.log('Choose a node first!');
+        }
+    }
+    jsonViewSelectSection.querySelector('.delete').onclick = deleteMainTree;
+    
+    function treeCommentUpdate(){
+        if(languageData && currentSelected ){
+            var sentenceObj = languageData[currentSelected[0]];
+            var sentenceLangObj = sentenceObj[chosenLanguage];
+            var treeObj = sentenceLangObj[currentSelected[1]];
+            treeObj.comment = jsonDrawSelectSection.querySelector('textarea').value;
+        }
+    }
+    jsonDrawSelectSection.querySelector('.tree-comment').onclick = treeCommentUpdate;
+
     function saveParser(e){
         e.preventDefault();
 
         var text = document.querySelector("textarea");
-        var blob = new Blob([JSON.stringify(languageData)], {
+        var blob = new Blob([JSON.stringify(languageData,
+                                          function(key, value){if(key == 'id'){return undefined} return value;})], {
         "type": "application/json"
         });
 
@@ -104,10 +208,49 @@ if (window.File && window.FileReader && window.FileList && window.Blob) {
         //text.value = "";
         //input.value = "";
         document.body.removeChild(a);
+        modifiedHistory = [];
     }
+    jsonViewSelectSection.querySelector('.savejson').onclick = saveParser;
     
-    document.querySelector('#load').addEventListener('change', handleFileSelect, false);
-    document.querySelector('#save').onclick = saveParser;    
+    function addTranslationTree(){
+        if(currentSelected){
+            var sentenceObj = languageData[currentSelected[0]];
+            var sentenceLangObj = sentenceObj[chosenLanguage];
+            var treeObj = sentenceLangObj[currentSelected[1]];
+            
+            var tempData = JSON.stringify(treeObj);
+            sentenceLangObj.splice(currentSelected[1], 0, JSON.parse(tempData));
+            
+            buildList(languageData, chosenLanguage);
+            showGraph(currentSelected);
+        } else{
+            console.log('Choose a node first!');
+        }
+    }
+    jsonDrawSelectSection.querySelector('.add').onclick = addTranslationTree;
+    
+    function deleteTranslationTree(){
+        if(currentSelected){
+            var sentenceObj = languageData[currentSelected[0]];
+            var sentenceLangObj = sentenceObj[chosenLanguage];
+            
+            if(sentenceLangObj.length > 1){
+                sentenceLangObj.splice(currentSelected[1], 1);
+            
+                buildList(languageData, chosenLanguage);
+                showGraph(currentSelected);
+            } else{
+                console.log('Only one translate tree available !!')
+            }
+            
+            
+        } else{
+            console.log('Choose a node first!');
+        }
+    }
+    jsonDrawSelectSection.querySelector('.delete').onclick = deleteTranslationTree;
+    
 } else {
   alert("Your browser is too old to support HTML5 File API");
 }
+
